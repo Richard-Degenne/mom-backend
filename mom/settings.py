302eb6d6e1a354131b07332f30b18bb70b9821fa
Richard-Degenne <richdeg2@gmail.com>
2016-05-11 17:14:41 +0200

    Set up development environment
    Database settings have been moved to `mom/secrets.py`, which is
    *git-ignored* for security purposes.

--- mom/settings.py
+++ mom/settings.py
@@ -12,6 +12,8 @@ https://docs.djangoproject.com/en/1.9/ref/settings/
 
 import os
 
+from .secrets import * # Contains sensitive data
+
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
@@ -74,13 +76,7 @@ WSGI_APPLICATION = 'mom.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/1.9/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    }
-}
-
+# Moved to secrets.py for security purposes
 
 # Password validation
 # https://docs.djangoproject.com/en/1.9/ref/settings/#auth-password-validators
